3b874503f37cee4d00c21f33f2b4c08434962244
Joop Stark <stark.joop@gmail.com>
2023-11-14 16:14:52 -0700

    refactor user_facade

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,5 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: UserSerializer.new(user), status: :created
-    else
-      # binding.pry
-      render json: ErrorSerializer.new(user.errors.messages).serialize_json , status: 409
-    end
-  end
-
-  private
-
-  def user_params
-    params.permit(:name, :email, :password, :password_confirmation) 
+    render UserFacade.create(params) 
   end
 end
