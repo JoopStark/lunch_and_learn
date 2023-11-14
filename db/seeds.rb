0687bd812f60c985ebad5a4dee2de26721ad63bb
Joop Stark <stark.joop@gmail.com>
2023-11-14 09:59:59 -0700

    session request tests complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!( name: "Dave", email: 'dave@aol.com', password: 'Password12?', api_key: 111222333)
